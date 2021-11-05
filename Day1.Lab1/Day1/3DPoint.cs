3e0e93dd1c35d1756b12b4e45abce2c906cf7a86
Ahmed Mamdouh Ismael <73735102+ahmadmamdouh-10@users.noreply.github.com>
2021-11-05 14:46:35 +0200

    Add files via upload

---
+++ Day1.Lab1/Day1/3DPoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1.Lab1
+{
+    class point
+    {
+        public int x { set; get; }
+        public int y { set; get; }
+        public int z { set; get; }
+        public void displayAll()
+        {
+            Console.WriteLine($"value of x =  {x} \t , value of y =  {y} \t , value of z = {z}");
+        }
+
+        public int CalcDistance(point p1)
+        {
+            int distance;
+            double partx = (int)Math.Pow((x - p1.x), 2);
+            distance = (int)Math.Sqrt(partx);
+            return distance;
+        }
+
+    }
+}
---
+++ Day1.Lab1/Day1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day1.Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            point p1 = new point() { x = 50, y = 75, z = 90 };
+            point p2 = new point() { x = 70, y=65, z= 80 };
+
+
+            p1.displayAll();
+
+            p2.displayAll();
+
+           Console.WriteLine($"the result of calculation distance = {p2.CalcDistance(p1)} ");
+        }
+    }
+}
