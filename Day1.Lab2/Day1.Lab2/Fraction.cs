4b242176a70b5d9ebdfe15b243078d70eaf2cc80
Ahmed Mamdouh Ismael <73735102+ahmadmamdouh-10@users.noreply.github.com>
2021-11-05 14:47:16 +0200

    Add files via upload

---
+++ Day1.Lab2/Day1.Lab2/Fraction.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1.Lab2
+{
+    class Fraction
+    {
+        public int nom { set; get; }
+        private int den;
+
+        public int Den
+        {
+            set
+            {
+                if (value == 0)
+                {
+                    den = 1;
+                    Console.WriteLine("Invalid Denominator, we will replace it to 1 \n");
+                    
+                }
+                else
+                {
+                    den = value;
+                    
+                }
+
+            }
+
+            get
+            {
+                return den;
+
+            }
+        }
+       
+
+       public  void display() {
+            Console.WriteLine($"Nominator = {nom} \n Denominator = {den} ");
+        }
+
+        public Fraction add(Fraction f1, Fraction f2)
+        {
+            Fraction res= new Fraction();
+            res.nom = (f1.nom * f2.Den) + (f2.nom * f1.Den);
+            res.Den = f1.den * f2.den;
+
+            return res;
+
+        }
+
+
+
+    }
+}
---
+++ Day1.Lab2/Day1.Lab2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Day1.Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction f1 = new Fraction() {nom = 5, Den = 6 };
+            Fraction f2 = new Fraction() { nom = 2, Den = 5 };
+
+           
+
+            f1.display();
+
+          
+
+            f2.display();
+
+
+            Fraction res = f1.add(f1, f2);
+            Console.WriteLine($"The Sum = {res.nom} / {res.Den} ");
+
+        }
+    }
+}
